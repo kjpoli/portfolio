edf687e374e4dec0f15635149d05e5767491e4a3
Kevin Poli <kevinpoli@Kevins-iMac-Pro.greengate.net>
2019-01-07 19:45:40 -0500

    Updates

---
+++ precache-manifest.3c808ccf06b98e4f28410902ab29405c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a2ca8db14d2b9059e37",
+    "url": "/portfolio-gh-pages/static/css/main.6c253601.chunk.css"
+  },
+  {
+    "revision": "2a2ca8db14d2b9059e37",
+    "url": "/portfolio-gh-pages/static/js/main.2a2ca8db.chunk.js"
+  },
+  {
+    "revision": "7affd7b41c17b84fcfcd",
+    "url": "/portfolio-gh-pages/static/js/1.7affd7b4.chunk.js"
+  },
+  {
+    "revision": "3efcc0305d4cff12aede",
+    "url": "/portfolio-gh-pages/static/js/runtime~main.3efcc030.js"
+  },
+  {
+    "revision": "46d635c53dfb6a92edc721c44f50a7c5",
+    "url": "/portfolio-gh-pages/index.html"
+  }
+];
